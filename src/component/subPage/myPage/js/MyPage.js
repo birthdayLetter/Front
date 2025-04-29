431e85b51430512d49658b2330de621a9e2535d2
GaRyung <shubom0115@naver.com>
2025-04-29 20:29:20 +0900

    MyPage infoModify 수정중

--- src/component/subPage/myPage/js/MyPage.js
+++ src/component/subPage/myPage/js/MyPage.js
@@ -9,12 +9,21 @@ const MyPage = () => {
     const didAlert = useRef(false);
     const [userProfile, setUserProfile] = useState({
         name: '',
+        userid: '',
+        email: '',
+        imagePath: '',
+        description: ''
+    });
+    const [modifyUser, setModifyUser] = useState({
+        name: '',
+        userid: '',
         email: '',
         password: '',
-        phoneNumber: '',
-        adress: '',
-        imagePath: ''
+        imagePath: '',
+        description: ''
     });
+
+    const [modifyBtn, setModifyBtn] = useState(false);
 
     useEffect(() => {
         if (didAlert.current) return; // 이미 알럿했으면 무시
@@ -35,15 +44,29 @@ const MyPage = () => {
             })
             const json = await res.json();
             console.log(json);
+
             if (res.ok) {
-                setUserProfile({
-                    ...userProfile,
-                    name: json.name,
-                    email: json.email,
-                    password: json.password,
-                    birthDay: json.birthDay,
-                    imagePath: json.profileUrl,
-                });
+                if (json.description === null) {
+                    setUserProfile({
+                        ...userProfile,
+                        name: json.name,
+                        userid: json.userId,
+                        email: json.email,
+                        birthDay: json.birthDay,
+                        imagePath: json.profileUrl,
+                        description: '아직 소개글을 작성하지 않았습니다.'
+                    });
+                } else {
+                    setUserProfile({
+                        ...userProfile,
+                        name: json.name,
+                        userid: json.userId,
+                        email: json.email,
+                        birthDay: json.birthDay,
+                        imagePath: json.profileUrl,
+                        description: json.description
+                    });
+                }
             } else {
                 console.error('응답 상태 코드:', res.status);
                 alert('서버와의 통신이 원활하지 않습니다. 상태 코드: ' + res.status);
@@ -60,19 +83,38 @@ const MyPage = () => {
             const json = await res.json();
             console.log(json);
             if (res.ok) {
-                setUserProfile({
-                    ...userProfile,
-                    name: json.name,
-                    email: json.email,
-                    password: json.password,
-                    birthDay: json.birthDay,
-                    imagePath: json.profileUrl,
-                });
+                if (json.description === null) {
+                    setUserProfile({
+                        ...userProfile,
+                        name: json.name,
+                        userid: json.userId,
+                        email: json.email,
+                        birthDay: json.birthDay,
+                        imagePath: json.profileUrl,
+                        description: '아직 소개글을 작성하지 않았습니다.'
+                    });
+
+                } else {
+                    setUserProfile({
+                        ...userProfile,
+                        name: json.name,
+                        userid: json.userId,
+                        email: json.email,
+                        birthDay: json.birthDay,
+                        imagePath: json.profileUrl,
+                        description: json.description
+                    });
+                }
             } else {
                 console.error('응답 상태 코드:', res.status);
                 alert('서버와의 통신이 원활하지 않습니다. 상태 코드: ' + res.status);
             }
         }
+    }
+
+    // 정보 수정을 눌렀을때
+    const modifyHandler = () => {
+        setModifyBtn(!modifyBtn);
     }
 
     return(
@@ -81,32 +123,81 @@ const MyPage = () => {
             <div className="mypage-container">
                 <div className="mypage-box">
                     <div className="myinfo-container">
-                        <div className="myprifile-img"></div>
+                        <div className="myprifile-img">
+                            <img src={userProfile.imagePath} alt=""/>
+                        </div>
                         <div className="myex-box">
-                            <div className="myinfo-box myintro-info">
-                                <p>자기소개</p>
-                                <div className="myintro">하이하이</div>
-                            </div>
-                            <div className="myinfo-box myname-info">
-                                <p>이름</p>
-                                <div className="myname">하이하이</div>
-                            </div>
-                            <div className="myinfo-box myemail-info">
-                                <p>이메일</p>
-                                <div className="myemail">하이하이</div>
-                            </div>
-                            <div className="myinfo-box mypw-info">
-                                <p>비밀번호</p>
-                                <div className="mypw">********</div>
-                            </div>
-                            <div className="myinfo-box mybirth-info">
-                                <p>생년월일</p>
-                                <div className="mybirth">s</div>
-                            </div>
+                            {modifyBtn ? (
+                                <>
+                                    <div className="myinfo-box myintro-info">
+                                        <p>자기소개</p>
+                                        <div className="myintro">{userProfile.description}</div>
+                                    </div>
+                                    <div className="myinfo-box myname-info">
+                                        <p>이름</p>
+                                        <div className="myname">{userProfile.name}</div>
+                                    </div>
+                                    <div className="myinfo-box myuserid-info">
+                                        <p>유저 ID</p>
+                                        <div className="myuserid">{userProfile.userid}</div>
+                                    </div>
+                                    <div className="myinfo-box myemail-info">
+                                        <p>이메일</p>
+                                        <div className="myemail">{userProfile.email}</div>
+                                    </div>
+                                    <div className="myinfo-box mypw-info">
+                                        <p>비밀번호</p>
+                                        <div className="mypw">********</div>
+                                    </div>
+                                    <div className="myinfo-box mybirth-info">
+                                        <p>생년월일</p>
+                                        <div className="mybirth">{userProfile.birthDay}</div>
+                                    </div>
+
+                                </>
+                            ) : (
+                                <>
+                                    <div className="myinfo-box myintro-info">
+                                        <p>자기소개</p>
+                                        <input className="modify-input myintro" placeholder={userProfile.description}/>
+                                    </div>
+                                    <div className="myinfo-box myname-info">
+                                        <p>이름</p>
+                                        <input className="modify-input myname" placeholder={userProfile.name}/>
+                                    </div>
+                                    <div className="myinfo-box myuserid-info">
+                                        <p>유저 ID</p>
+                                        <input className="modify-input myuserid" placeholder={userProfile.userid}/>
+                                    </div>
+                                    <div className="myinfo-box myemail-info">
+                                        <p>이메일</p>
+                                        <input className="modify-input myemail" placeholder={userProfile.email}/>
+                                    </div>
+                                    <div className="myinfo-box mypw-info">
+                                        <p>비밀번호</p>
+                                        <input className="modify-input mypw" placeholder={'********'}/>
+                                    </div>
+                                    <div className="myinfo-box mybirth-info">
+                                        <p>생년월일</p>
+                                        <input className="modify-input mybirth" placeholder={userProfile.birthDay}/>
+                                    </div>
+
+                                </>
+                            )}
 
                         </div>
                     </div>
-                    <button className="modify-box">정보수정</button>
+                    {modifyBtn ? (
+                        <>
+                            <button className="modifybtn" onClick={modifyHandler}>정보수정</button>
+                        </>
+                    ) : (
+                        <div className="modify-box">
+                            <button className="modifybtn" onClick={modifyHandler}>취소</button>
+                            <button className="modifybtn">확인</button>
+                        </div>
+                    )}
+
                 </div>
             </div>
         </>
