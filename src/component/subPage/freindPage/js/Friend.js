d5aebae214be4ae9662c728ef7df8a0fceff7e4b
GaRyung <shubom0115@naver.com>
2025-05-10 15:45:13 +0900

    list 불러오기, 친구찾기 log로만 찍히도록 수정

--- src/component/subPage/freindPage/js/Friend.js
+++ src/component/subPage/freindPage/js/Friend.js
@@ -1,26 +1,67 @@
 import '../scss/Friend.scss';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {FRIEND_URL} from "../../../../config/host-config.js";
 import Header from "../../../header/js/Header.js";
 import FriendList from "../js/FriendList.js";
 import { HiSearch } from "react-icons/hi";
-import { BsPersonPlusFill } from "react-icons/bs";
+import {BsPersonFillAdd, BsPersonFillCheck} from "react-icons/bs";
 
 const Friend = () => {
     const FRIEND_SEARCH_URL = FRIEND_URL + '/search';
-    const [modal, setModal] = useState(false);
+    const FRIEND_LIST_URL = FRIEND_URL + '/list';
+    const localToken = localStorage.getItem('ACCESS_TOKEN');
+    const sessionToken = sessionStorage.getItem('ACCESS_TOKEN');
+    const [friendList, setFriendList] = useState([]);
+    const displayFriendList = friendList.slice(
+        // (page - 1) * itemsPerPage,
+        // page * itemsPerPage
+    );
+    const [addModal, setAddModal] = useState(false);
+    const [checkModal, setCheckModal] = useState(false);
     const modalBackground = useRef();
     const [frInput, setFrInput] = useState();
 
+    useEffect(() => {
+        fetchFriendList();
+    }, []);
+
+
     const friendAddClick = () => {
-        setModal(true);
+        setAddModal(true);
+    }
 
-
+    const friendCheckClick = () => {
+        setCheckModal(true);
     }
 
     const searchFrInput = (e) => {
         const inputVal = e.target.value;
         setFrInput(inputVal);
+    }
+
+    const fetchFriendList = async () => {
+        const tokenToUse = sessionToken || localToken;
+
+            const res = await fetch(FRIEND_LIST_URL, {
+                method: 'GET',
+                headers: {
+                    'X-AUTH-TOKEN': tokenToUse, // 인증 헤더 추가
+                    'Content-Type': 'application/json',
+                },
+
+            })
+            const json = await res.json();
+            // console.log(json);
+            if (res.ok) {
+                console.log(json);
+                setFriendList(json);
+
+            } else {
+                console.error('응답 상태 코드:', res.status);
+                alert('서버와의 통신이 원활하지 않습니다. 상태 코드: ' + res.status);
+            }
+
+
     }
 
     const searchFreind = async () => {
@@ -41,8 +82,6 @@ const Friend = () => {
             console.error('응답 상태 코드:', res.status);
             alert('서버와의 통신이 원활하지 않습니다. 상태 코드: ' + res.status);
         }
-
-
     }
 
     return (
@@ -55,10 +94,25 @@ const Friend = () => {
                         <HiSearch className='friend-icon'/>
                     </div>
                     <div className="friend-add">
-                        <BsPersonPlusFill className='friend-icon' onClick={friendAddClick}/>
+                        <BsPersonFillAdd className='friend-icon' onClick={friendAddClick}/>
+                        <BsPersonFillCheck className='friend-icon' onClick={friendCheckClick}/>
                     </div>
                 </div>
                 <ul className="friend-list">
+
+                    {displayFriendList.map((boards, index) => (
+                        <FriendList
+                            key={index}
+                            id={boards.id}
+                            thumbnailUrl={boards.thumbnailUrl}
+                        //     title={boards.title}
+                        //     content={boards.content}
+                        //     author={boards.author}
+                        //     likeScore={boards.likeScore}
+                        //     tag={boards.tag}
+                        //     createdDate={boards.createdDate}
+                        />
+                    ))}
 
                     <FriendList/>
 
@@ -66,14 +120,14 @@ const Friend = () => {
 
             </div>
             {
-                modal &&
+                addModal &&
                 <div className={'modal-container'} ref={modalBackground} onClick={e => {
                     if (e.target === modalBackground.current) {
-                        setModal(false);
+                        setAddModal(false);
                     }
                 }}>
                     <div className={'modal-content'}>
-                        <div className="modal-header">
+                        <div className="add-modal-header">
                             <p className="modal-title">친구 찾기</p>
                             {/*<button className={'modal-close-btn'} onClick={() => setModal(false)}>*/}
                             {/*    모달 닫기*/}
@@ -97,11 +151,30 @@ const Friend = () => {
                 </div>
             }
 
-            {/*<div className="modal-container">*/}
-            {/*    <div className="friend-add-modal">*/}
-
-            {/*    </div>*/}
-            {/*</div>*/}
+            {
+                checkModal &&
+                <div className={'modal-container'} ref={modalBackground} onClick={e => {
+                    if (e.target === modalBackground.current) {
+                        setCheckModal(false);
+                    }
+                }}>
+                    <div className={'modal-content'}>
+                        <div className="check-modal-header">
+                            <p className="modal-title">친구 요청</p>
+                        </div>
+                        {/*<div className="modal-friend-container">*/}
+                        {/*    <div className="fr-box">*/}
+                        {/*        <div className="fr-profile"></div>*/}
+                        {/*        <div className="fr-info">*/}
+                        {/*            <div className="fr-name">홍길동</div>*/}
+                        {/*            <div className="fr-des">안뇽</div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <button className="fr-send">add+</button>*/}
+                        {/*</div>*/}
+                    </div>
+                </div>
+            }
 
         </>
     )
